e7f1fdb1b3083b86108ce7c283ed8635e54aa654
andres julian barrera diaz <ing.julian.26@gmail.com>
2021-01-25 21:23:50 -0500

    Taller1_21_01_2021

---
+++ pedir_Datos_Por_Prompt_21_01_2021/main.js
@@ -0,0 +1,2 @@
+let numero = prompt("Ingresar un numero:");
+alert("El numero es : "+ numero);
---
+++ todo_Sobre_Variables_21_01_2021/main.js
@@ -0,0 +1,9 @@
+let nombre= "andres barrera";
+let edad="27";
+let soy_desarrollador=true;
+const array={id:1, nombre:"andres"};
+
+alert("Nombre:" + nombre);
+alert("Edad:" + edad);
+alert("Soy_desarrollador:" + soy_desarrollador);
+alert("Array:" + array);
